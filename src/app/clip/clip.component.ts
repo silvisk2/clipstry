0a51f7d938698839fd9c4818232ff6503df309be
Silviu CAPRARU <scapraru@bitdefender.com>
2023-02-09 00:48:34 +0200

    try

---
+++ src/app/clip/clip.component.ts
@@ -0,0 +1,32 @@
+import  videojs  from 'video.js';
+import  IClip from 'src/app/models/clip.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { DatePipe } from '@angular/common';
+@Component({
+  selector: 'app-clip',
+  templateUrl: './clip.component.html',
+  styleUrls: ['./clip.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe]
+})
+export class ClipComponent implements OnInit {
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef
+  player?: videojs.Player
+  clip?: IClip
+
+  constructor(public route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement)
+
+    this.route.data.subscribe(data => {
+      this.clip = data['clip'] as IClip
+
+      this.player?.src({
+        src: this.clip.url,
+        type: 'video/mp4'
+      })
+    })
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCgEPSDuwZHJ4ML934kP3yuKUuPuojMIdA",
+    authDomain: "clips-409f1.firebaseapp.com",
+    projectId: "clips-409f1",
+    storageBucket: "clips-409f1.appspot.com",
+    // messagingSenderId: "1024336354572",
+    appId: "1:1024336354572:web:e64c55f1373782ac2bfd29"
+  }
+};
